33ded15fca094eca02364241583ddd1925b4c144
Cassetu <cassetu@example.com>
2025-05-24 09:35:09 -0700

    Start

---
+++ src/main/java/cassetu/solarium/SolariumClient.java
@@ -0,0 +1,12 @@
+package cassetu.solarium;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class SolariumClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
